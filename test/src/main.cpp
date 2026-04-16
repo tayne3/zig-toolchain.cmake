84bf9a2a7c5e3474f581da14402e7f9bedb90809
tayne3 <tayne3@dingtalk.com>
2026-04-16 15:08:23 +0800

    chore: improve test items

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -3,9 +3,24 @@
 #include "shared_lib.h"
 #include "static_lib.h"
 
+#ifndef PROJECT_VERSION
+#define PROJECT_VERSION "unknown"
+#endif
+#ifndef BUILD_DATE
+#define BUILD_DATE "unknown"
+#endif
+#ifndef BUILD_COMPILER_ID
+#define BUILD_COMPILER_ID "unknown"
+#endif
+#ifndef BUILD_COMPILER_VERSION
+#define BUILD_COMPILER_VERSION "unknown"
+#endif
+
 int main(void) {
     int a = 1, b = 2;
     printf("%d + %d = %d/%d\n", a, b, add(a, b), shared_lib::SharedLib::add(a, b));
     printf("%d - %d = %d/%d\n", a, b, sub(a, b), static_lib::StaticLib::sub(a, b));
+
+    printf(PROJECT_VERSION " " BUILD_DATE " (" BUILD_COMPILER_ID " " BUILD_COMPILER_VERSION ").\n");
     return 0;
 }
